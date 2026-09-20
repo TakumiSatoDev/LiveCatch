de899b30bae0ca3bdb345dc5a6fd57bef8633ba4
Satotaku <tashizuoteng9@gmail.com>
2026-09-21 08:17:24 +0900

    test: cover 256 fragment concurrency

--- tests/test_core.py
+++ tests/test_core.py
@@ -23,7 +23,7 @@ def test_urls(url):
 def test_bad_urls(url):
     with pytest.raises(ValueError): normalize_url(url)
 
-@pytest.mark.parametrize('changes', [dict(concurrent_fragments=0),dict(concurrent_fragments=129),dict(concurrent_fragments=True),dict(prefetch=0),dict(prefetch=9),dict(language='xx'),dict(gpu_export='magic'),dict(gpu_jobs=9),dict(gpu_preset='warp'),dict(export_height=17),dict(wait_seconds=1),dict(mode='catchup_stop',engine='stock'),dict(save_dir=''),dict(use_temp_dir=True,temp_dir='')])
+@pytest.mark.parametrize('changes', [dict(concurrent_fragments=0),dict(concurrent_fragments=257),dict(concurrent_fragments=True),dict(prefetch=0),dict(prefetch=9),dict(language='xx'),dict(gpu_export='magic'),dict(gpu_jobs=9),dict(gpu_preset='warp'),dict(export_height=17),dict(wait_seconds=1),dict(mode='catchup_stop',engine='stock'),dict(save_dir=''),dict(use_temp_dir=True,temp_dir='')])
 def test_validation(changes):
     with pytest.raises(ValueError): replace(Settings(url='https://youtu.be/test'),**changes).validate()
 
@@ -37,7 +37,7 @@ def test_legacy_gpu_export_is_disabled_on_load():
 
 
 def test_extreme_limits_are_explicitly_allowed():
-    replace(Settings(url='https://youtu.be/test'), concurrent_fragments=128, prefetch=8,
+    replace(Settings(url='https://youtu.be/test'), concurrent_fragments=256, prefetch=8,
             gpu_jobs=8, gpu_preset='max_speed').validate()
 
 def test_migration_atomic_preserve(tmp_path):
